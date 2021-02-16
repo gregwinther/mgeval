458e04c5bdddf09efde6d0c0a802ae9c9be45d97
gregwinther <sebastwi@uio.no>
2021-02-16 15:04:52 +0100

    Python 3 refactor

--- mgeval/core.py
+++ mgeval/core.py
@@ -254,7 +254,7 @@ class metrics(object):
             return transition_matrix / sum(sum(transition_matrix))
 
         else:
-            print "invalid normalization mode, return unnormalized matrix"
+            print("invalid normalization mode, return unnormalized matrix")
             return transition_matrix
 
     def pitch_range(self, feature):
@@ -534,7 +534,7 @@ class metrics(object):
             return transition_matrix / sum(sum(transition_matrix))
 
         else:
-            print "invalid normalization mode, return unnormalized matrix"
+            print("invalid normalization mode, return unnormalized matrix")
             return transition_matrix
 
     # def chord_dependency(self, feature, bar_chord, bpm=120, num_bar=None, track_num=1):
